7fd69f1f11e9a314b009c6bc45b855fd9b35ebc9
Emmanuel Rivero <alu0100763249@ull.edu.es>
2014-11-19 11:45:16 +0000

    Clase Examen creada

--- lib/exam/examen.rb
+++ lib/exam/examen.rb
@@ -1,4 +1,13 @@
 class Examen
-    
+
+ attr_accessor :exam, :npreguntas
+    def initialize(n)
+      @exam= List.new
+      @npreguntas=n
+    end
+   
+    def add_pregunta(pre)
+        @exam.push(pre)
+    end
    
 end
